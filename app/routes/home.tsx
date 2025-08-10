814f509f4555805ecb9daf1b998f7b408f8f9e57
Talat Bayram <talat.bayram1@gmail.com>
2025-08-10 16:57:27 +0300

    Implement wipe functionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,22 +2,25 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
 import {usePuterStore} from "~/lib/puter";
-import {useNavigate} from "react-router";
+import {Link, useNavigate} from "react-router";
 import {useEffect, useState} from "react";
-import {Link} from "react-router/internal/react-server-client";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Resumind" },
-    { name: "description", content: "Smart feedback for your dream job!" },
-  ];
+    return [
+        { title: "Resumind" },
+        { name: "description", content: "Smart feedback for your dream job!" },
+    ];
 }
 
 export default function Home() {
-  const { auth, kv } = usePuterStore();
-  const navigate = useNavigate();
+    const { auth, kv } = usePuterStore();
+    const navigate = useNavigate();
     const [resumes, setResumes] = useState<Resume[]>([]);
     const [loadingResumes, setLoadingResumes] = useState(false);
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
 
     useEffect(() => {
         const loadResumes = async () => {
@@ -29,45 +32,46 @@ export default function Home() {
                 JSON.parse(resume.value) as Resume
             ))
 
-            console.log("parsedResumes", parsedResumes);
             setResumes(parsedResumes || []);
             setLoadingResumes(false);
         }
 
-        loadResumes();
-    }, [auth.isAuthenticated]);
+        loadResumes()
+    }, []);
 
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar/>
-    <section className="main-section">
-    <div className="page-heading py-16">
-      <h1>Track Your Applications & Resume Ratings</h1>
-        {!loadingResumes && resumes?.length === 0 ?(
-            <h2>No resumes found. Upload your first resume to get feedback.</h2>
-        ):(
-            <h2>Review your submissions and check AI-powered feedback.</h2>
-        )}
-    </div>
-        {loadingResumes && (
-            <div className="flex flex-col justify-center items-center">
-                <img src="/images/resume-scan.gif" className="w-[200px]"/>
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar />
+
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Track Your Applications & Resume Ratings</h1>
+                {!loadingResumes && resumes?.length === 0 ? (
+                    <h2>No resumes found. Upload your first resume to get feedback.</h2>
+                ): (
+                    <h2>Review your submissions and check AI-powered feedback.</h2>
+                )}
             </div>
-        )}
-    {!loadingResumes && resumes.length > 0 && (
-        <div className="resumes-section">
-          {resumes.map((resume) => (
-              <ResumeCard key={resume.id} resume={resume}/>
-          ))}
+            {loadingResumes && (
+                <div className="flex flex-col items-center justify-center">
+                    <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+                </div>
+            )}
+
+            {!loadingResumes && resumes.length > 0 && (
+                <div className="resumes-section">
+                    {resumes.map((resume) => (
+                        <ResumeCard key={resume.id} resume={resume} />
+                    ))}
+                </div>
+            )}
 
             {!loadingResumes && resumes?.length === 0 && (
-                <div className="flex flex-col justify-center items-center mt-10 gap-4">
+                <div className="flex flex-col items-center justify-center mt-10 gap-4">
                     <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
                         Upload Resume
                     </Link>
                 </div>
             )}
-        </div>
-    )}
-    </section>
-  </main>
+        </section>
+    </main>
 }
